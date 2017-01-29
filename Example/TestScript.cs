561b09b43ed81a0d08c80295873febc00c56a0c0
Dan <dfsilk@gmail.com>
2017-01-29 15:30:52 +1030

    Fixed issue in example where colour would not change as the equations weren’t floats

--- Example/TestScript.cs
+++ Example/TestScript.cs
@@ -1,31 +1,34 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
-public class TestScript : MonoBehaviour {
+[RequireComponent (typeof(MeshRenderer))]
+public class TestScript : MonoBehaviour
+{
 
 	// Use this for initialization
 	void Start () {
 
-		QuakeConsole.RegisterCommand ("floorCol", ChangeFloorColor, "[r g b] Change the floor color as integers from 0 - 255");
+		DropConsole.RegisterCommand ("floorCol", ChangeFloorColor, "[r g b] Change the floor color as integers from 0 - 255");
 	}
-	
-	string ChangeFloorColor(params string[] args) {
+
+	string ChangeFloorColor (params string[] args) {
 
 		if (args.Length == 3) {
 			int r, g, b;
 
-			if (int.TryParse(args[0], out r) && int.TryParse(args[1], out g) && int.TryParse(args[2], out b)) {
+			if (int.TryParse (args [0], out r) && int.TryParse (args [1], out g) && int.TryParse (args [2], out b)) {
 
-				var newColor = new Color (r / 255, g / 255, b / 255);
+				var newColor = new Color (r / 255f, g / 255f, b / 255f);
 				var meshRenderer = gameObject.GetComponent<MeshRenderer> ();
 
 				if (meshRenderer != null) {
 
 					meshRenderer.material.color = newColor;
 
-					return "Floor colour changed";
+					return "Floor colour changed to " + newColor.ToString ();
 				}
 			}
+
+			return "Could not parse colours";
 		}
 
 		return "Arguments are not correct";
